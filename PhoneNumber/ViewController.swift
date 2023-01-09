7b5546f9d45573d43dba3b11622affc0acf8c472
faxriddin0516 <faxaoff@gmail.com>
2023-01-09 10:38:38 +0500

    Initial Commit

---
+++ PhoneNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneNumber
+//
+//  Created by MacAir on 09/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
