028ac535bdd861c07c1daf6f52a0bb83a535143e
faxriddin0516 <faxaoff@gmail.com>
2023-01-10 14:34:18 +0500

    PhoneNumberFormater

--- PhoneNumber/ViewController.swift
+++ PhoneNumber/ViewController.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let  text = textField.text
+        else {
+            return false
+        }
+        let newString = (text as NSString).replacingCharacters(in: range, with: string)
+        textField.text = formatter(mask: "+XXX (XX) XXX XX XX", phoneNumber: newString)
+        return false
+    }
+    
+    func formatter(mask: String , phoneNumber: String) -> String {
+        let number = phoneNumber.replacingOccurrences(of: "[^0-9]", with: "" , options: .regularExpression)
+        var result: String = ""
+        var index = number.startIndex
+        
+        for character in mask where index < number.endIndex {
+            if character == "X" {
+                result.append(number[index])
+                index = number.index(after: index)
+            }
+            else {
+                result.append(character)
+            }
+        }
+        return result
     }
 
 
